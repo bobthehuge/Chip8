d100316312921537ba087b9acf36242d568bc4da
bobthehuge <bobthehuge@protonmail.com>
2024-04-04 20:37:33 +0200

    WIP

--- chip.c
+++ chip.c
@@ -1,9 +1,11 @@
-#define ROM_PATH    "roms/sierpinsky.ch8"
-#define MEM_SIZE    4096
-#define RESERVED    512
-#define MAX_LVL     16
-#define FONT_SIZE   16
-#define CHR_SIZE    5
+#define ROM_PATH        "roms/sierpinsky.ch8"
+#define MEM_SIZE        4096
+#define RESERVED        512
+#define MAX_LVL         16
+#define FONT_SIZE       16
+#define CHR_SIZE        5
+#define SCREEN_WIDTH    64
+#define SCREEN_HEIGHT   32
 
 #define DEBUG_ON
 
@@ -15,6 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "log.h"
 
@@ -35,12 +38,17 @@ struct Chip
     byte reg[16];
     word pc;
     byte sp;
+    word I;
 
     byte sound;
     byte timer;
 
+    int redraw;
+
     word stack[MAX_LVL];
     byte mem[MEM_SIZE];
+    byte gfx[SCREEN_WIDTH * SCREEN_HEIGHT];
+    byte key[16];
 
     enum ChipState state;
 };
@@ -124,12 +132,12 @@ void cyclechip(struct Chip *chip)
 {
     byte inst = chip->mem[chip->pc];
     byte x = inst & 0x0F;
-    inst &= 0xF0;
+    inst &= 0xF0 >> 4;
 
-    byte kk = chip->mem[chip->pc+1];
-    word nnn = ((word)x << 4) + kk; 
+    byte kk = chip->mem[chip->pc + 1];
+    word nnn = ((word)x << 8) + kk; 
     byte n = nnn & 0x000F;
-    byte y = nnn & 0x00F0;
+    byte y = nnn & 0x00F0 >> 4;
 
     word op = (word)inst << 8 | kk;
 
@@ -142,7 +150,7 @@ void cyclechip(struct Chip *chip)
         switch(kk)
         {
         case 0xE0:
-            // clear screen
+            memset(chip->gfx, 0, SCREEN_WIDTH * SCREEN_HEIGHT);
             chip->pc += 2;
             return;
 
@@ -155,7 +163,7 @@ void cyclechip(struct Chip *chip)
             return;
 
         default:
-            WARNX("Unknown instruction '0x%04X'\n", op);
+            WARNX("Unknown instruction '0x%04X' @ '0x%04X'\n", op, chip->pc);
             chip->state = ERR;
             return;
         }
@@ -239,18 +247,90 @@ void cyclechip(struct Chip *chip)
             break;
 
         case 0x05:
-            chip->reg[0xF] = (vY > vX) ? 0 : 1;
+            chip->reg[0xF] = (vX > vY) ? 1 : 0;
             chip->reg[(int)x] -= vY;
+            break;
+
+        case 0x06:
+            chip->reg[0xF] = (vX & 0x01);
+            chip->reg[(int)x] /= 2;
+            break;
+
+        case 0x07:
+            chip->reg[0xF] = (vX > vY) ? 1 : 0;
+            chip->reg[(int)x] = vY - vX;
+            break;
+
+        case 0x0E:
+            chip->reg[0xF] = (vX & 0x01);
+            chip->reg[(int)x] *= 2;
             break;
         }
 
         chip->pc += 2;
+        return;
+
+    case 0x09:
+        if (vX != vY)
+        {
+            chip->pc += 2;
+        }
+
+        chip->pc += 2;
+        return;
+
+    case 0x0A:
+        chip->I = nnn;
+        chip->pc += 2;
+        return;
+
+    case 0x0B:
+        chip->pc = nnn + chip->reg[0];
+        return;
+
+    case 0x0C:
+        chip->reg[(int)x] = (byte)(rand() % 0xFF) & kk;
+        chip->pc += 2;
+        return;
+
+    case 0x0D:
+        {
+            chip->reg[0xF] = 0;
+
+            for (int yline = 0; yline < n; yline++)
+            {
+                word pixel = chip->mem[chip->I + yline];
+
+                for (int xline = 0; xline < 8; xline++)
+                {
+                    if ((pixel & (0x80 >> xline)) != 0)
+                    {
+                        size_t idx = x + xline + ((y + yline) * SCREEN_WIDTH);
+
+                        if (chip->gfx[idx] == 1)
+                        {
+                            chip->reg[0xF] = 1;
+                        }
+
+                        chip->gfx[idx] ^= 1;
+                    }
+                }
+            }
+        }
+
+        chip->redraw = 1;
+        chip->pc += 2;
+        return;
+
+    default:
+        WARNX("Unknown instruction '0x%04X' @ '0x%04X'\n", op, chip->pc);
         return;
     }
 }
 
 int main(void)
 {
+    srand(time(NULL));
     struct Chip chip = {0};
 
     chip.pc = RESERVED;
@@ -259,6 +339,11 @@ int main(void)
 
     romfile2mem(&chip, ROM_PATH);
     LOGX("Loaded '%s' to Chip's RAM", ROM_PATH);
+
+    while (chip.state != ERR && chip.pc < MEM_SIZE)
+    {
+        cyclechip(&chip);
+    }
 
     /* memdump(stdout, &chip); */
 
